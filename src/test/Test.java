27297982ed19b3d228ce956cdb2c9d1113360f60
santoshkompally <santoshkumarkompally@gmail.com>
2015-11-24 17:20:39 -0600

    adding hashing as well as master key generation.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,8 @@
 package test;
 
 import java.io.File;
-import java.security.SecureRandom;
 import java.util.Arrays;
-import java.util.Base64;
+
 
 import decryption.DecryptFile;
 import encryption.EncrptFile;
@@ -15,35 +14,39 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		
 		Utils u=new Utils();
-		byte[] b= new byte[20];
-		b=u.generateKey(16); // key for encrypting the file.
-		EncrptFile e=new EncrptFile(b,128,"AES","AES");
-		System.out.println("Generated file enc key" + Arrays.toString(b));
-		// generating some random master key this should be generated from the user password.
-		SecureRandom s=SecureRandom.getInstance("SHA1PRNG");
-		byte[] seed=s.generateSeed(16);
-		byte[] key_master = new byte[16];
-		s.setSeed(seed);
-		s.nextBytes(key_master);// example key for encrypting the file encryption key.
-		System.out.println("Generation of master key sample done" + Arrays.toString(key_master));
-
-		//key is encrypted here using the master key.
-		byte[] encryptedKey=u.keyEncryptAndDecrypt(b, 0,key_master);
+		byte[] fileKey= new byte[20];
+		byte[]hashKey=new byte[20];
+		fileKey=u.generateKey(16); // key for encrypting the file.
+		hashKey=u.generateKey(16);
+		
+		System.out.println("Hash key generated is:" + new String(hashKey));
+		
+		EncrptFile e=new EncrptFile(fileKey,128,"AES","AES");
+		System.out.println("Generated file enc key" + Arrays.toString(fileKey));
+		
+		//key is encrypted here using the master key which is generated in the same function.
+		byte[] encryptedKey=u.generateMasterKey_EncryptAndDecrypt("santosh", 0, fileKey);
+		byte[] encryptedHash=u.generateMasterKey_EncryptAndDecrypt("santosh", 0, hashKey);
+		System.out.println("Enc hash key is:" + encryptedHash);
 		System.out.println("key encryption done" + Arrays.toString(encryptedKey) );
 		// key is decrypted here using the master key.
 		File inputFile = new File("/Users/santoshkompally/Desktop/7001462-google-wallpaper.jpg");
         File encryptedFile = new File("/Users/santoshkompally/Desktop/test.jpg");
         File decFile=new File("/Users/santoshkompally/Desktop/test_dec2.jpg");
+        // generating hash of the input file.
+        System.out.println("hash before encryption" + u.generateHash(inputFile, hashKey));        
         e.Encrypt(inputFile, encryptedFile);
-		System.out.println("File encryption done using the generated key (b)" + Arrays.toString(b));	
-		
-		byte[] decryptedKey=u.keyEncryptAndDecrypt(encryptedKey, 1,key_master);
+		System.out.println("File encryption done using the generated key (b)" + Arrays.toString(fileKey));	
+		byte[] decryptedKey=u.generateMasterKey_EncryptAndDecrypt("santosh", 1,encryptedKey);
 		System.out.println("key decyption done using the decrypted file key" + Arrays.toString(decryptedKey));
 		
 		// this value should be equal to b. In that case only file will be decrypted.
 		DecryptFile d=new DecryptFile(decryptedKey,128,"AES","AES");
 		d.Decrypt(encryptedFile, decFile);
+		byte[] decryptedHash=u.generateMasterKey_EncryptAndDecrypt("santosh", 1, encryptedHash);
+		System.out.println("Enc hash key is:" + decryptedHash);
 		System.out.println("File Decrytion done using the decrypted key.");
+		System.out.println("hash after decryption" + u.generateHash(decFile, decryptedHash)); 
 		
 	}
 	
--- src/utility/Utils.java
+++ src/utility/Utils.java
@@ -3,21 +3,37 @@ package utility;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64.Encoder;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Utils {
 	
 	
 	// This is the method for encrypting the file.
+	/**
+	 * @param transformation
+	 * @param cipherMode
+	 * @param inputFile
+	 * @param outputFile
+	 * @param key
+	 * @throws Exception
+	 */
 	public void encryptFile(String transformation,int cipherMode,File inputFile,File outputFile,byte[] key) throws Exception{
 		
 		try {
@@ -44,6 +60,10 @@ public class Utils {
 		}
 	}
 	
+	/**
+	 * @param length
+	 * @return
+	 */
 	public byte[] generateKey(int length){
 		
 		SecureRandom random;
@@ -62,6 +82,12 @@ public class Utils {
 	}
 
 	
+	/**
+	 * @param input
+	 * @param mode1
+	 * @param key
+	 * @return
+	 */
 	public byte[] keyEncryptAndDecrypt(byte[] input,int  mode1,byte[] key){
 		int mode;
 		String transformation="AES";
@@ -93,6 +119,88 @@ public class Utils {
 			e.printStackTrace();
 		}
 		return encryptedKey;
+		
+	}
+	
+	public byte[] generateMasterKey_EncryptAndDecrypt(String password,int mode1,byte[] key){
+		
+		byte[] salt = {
+	            (byte)0xc7, (byte)0x73, (byte)0x21, (byte)0x8c,
+	            (byte)0x7e, (byte)0xc8, (byte)0xee, (byte)0x99
+	        };
+		int mode;
+		
+		if(mode1 ==0 ){
+			mode=Cipher.ENCRYPT_MODE;
+		}else{
+			mode=Cipher.DECRYPT_MODE;		
+		}
+		
+		PBEKeySpec pbeKeySpec;
+        PBEParameterSpec pbeParamSpec;
+        SecretKeyFactory keyFac;
+        
+        int count=20;
+        byte[] EncDec=null;
+        // set the salt and the number of iterations.
+        try {
+			pbeParamSpec = new PBEParameterSpec(salt, count);
+			pbeKeySpec = new PBEKeySpec(password.toCharArray());
+			keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+			SecretKey pbeKey = keyFac.generateSecret(pbeKeySpec);
+			Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES");
+			pbeCipher.init(mode, pbeKey, pbeParamSpec);
+			EncDec = pbeCipher.doFinal(key);
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	return EncDec;
+	
+	}
+
+	public String generateHash(File inputFile,byte[] key){
+		  final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+		  String hashValue=null;
+		try {
+			
+			FileInputStream inputStream = new FileInputStream(inputFile);
+	        byte[] inputBytes = new byte[(int) inputFile.length()];
+	        inputStream.read(inputBytes);
+			SecretKeySpec signingKey = new SecretKeySpec(key, HMAC_SHA1_ALGORITHM);
+			Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+			mac.init(signingKey);
+			byte[] macBytes = mac.doFinal(inputBytes);
+		    hashValue = new String(macBytes,"UTF-8");
+		    inputStream.close();
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return hashValue;
 		
 	}
 	

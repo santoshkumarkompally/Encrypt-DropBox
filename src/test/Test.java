1ab50a7601266957115884d66c5a73c927ea809a
santoshkompally <santoshkumarkompally@gmail.com>
2015-11-26 17:28:42 -0600

    encrypting

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,7 @@ package test;
 import java.io.File;
 
 import decryption.DecryptFile;
+import encryption.EncrptFile;
 
 
 public class Test {
@@ -11,16 +12,16 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		
 		
-	//	EncrptFile e=new EncrptFile(128,"AES","AES","username","password");
-		DecryptFile d= new DecryptFile(128,"AES","AES","username","password");
+		EncrptFile e=new EncrptFile(128,"AES","AES","username","password");
+	//	DecryptFile d= new DecryptFile(128,"AES","AES","username","password");
 		
 		//key is encrypted here using the master key which is generated in the same function.
 		File inputFile = new File("/Users/santoshkompally/Desktop/7001462-google-wallpaper.jpg");
         File encryptedFile = new File("/Users/santoshkompally/Desktop/test.jpg");
         File decFile=new File("/Users/santoshkompally/Desktop/test_dec2.jpg");
              
-     //   e.Encrypt(inputFile, encryptedFile);
-          d.Decrypt(encryptedFile, decFile);
+        e.Encrypt(inputFile, encryptedFile);
+     //     d.Decrypt(encryptedFile, decFile);
 		
 		
 	}
